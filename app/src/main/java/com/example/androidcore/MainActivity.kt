9f391dca7b29563fb9c4ba76a00c93340917f2c3
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-01 12:12:22 +0500

    add id TextView

--- app/src/main/java/com/example/androidcore/MainActivity.kt
+++ app/src/main/java/com/example/androidcore/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.androidcore
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById<TextView>(R.id.Main_Text)
+
     }
+
 }
