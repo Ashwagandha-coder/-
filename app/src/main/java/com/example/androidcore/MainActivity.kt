85c12ce02dff9a26afeed268d048c55371909bbf
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-01 16:19:30 +0500

    add spanString object

--- app/src/main/java/com/example/androidcore/MainActivity.kt
+++ app/src/main/java/com/example/androidcore/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.androidcore
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +17,24 @@ class MainActivity : AppCompatActivity() {
 
         textView = findViewById<TextView>(R.id.Main_Text)
 
+        // получение текста в перем
+
+        val full_text = getString(R.string.agreement_full_text)
+        val confidental = getString(R.string.confidental_info)
+        val policity = getString(R.string.privacy_politicy)
+
+        val spannableString = SpannableString(full_text)
+
+        val confidentalClicable = object : ClickableSpan() {
+
+            override fun onClick(widget: View) {
+                SnackBar.make
+                TODO("Not yet implemented")
+            }
+
+        }
+
     }
-    
+
 
 }
