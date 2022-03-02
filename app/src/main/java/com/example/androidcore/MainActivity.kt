ed14233a08ace6f05aa4997b930350711ed33595
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-02 12:14:16 +0500

    add markup object with method onClick

--- app/src/main/java/com/example/androidcore/MainActivity.kt
+++ app/src/main/java/com/example/androidcore/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.androidcore
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableString
+import android.text.TextPaint
 import android.text.style.ClickableSpan
 import android.view.View
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,21 +22,29 @@ class MainActivity : AppCompatActivity() {
         // получение текста в перем
 
         val full_text = getString(R.string.agreement_full_text)
-        val confidental = getString(R.string.confidental_info)
         val policity = getString(R.string.privacy_politicy)
+        val confidental = getString(R.string.confidental_info)
 
         val spannableString = SpannableString(full_text)
 
-        val confidentalClicable = object : ClickableSpan() {
-
+        val confidentalClick = object : ClickableSpan() {
             override fun onClick(widget: View) {
-                SnackBar.make
+                Snackbar.make(widget, "Go to Link", SnackBar.LENGTH_SHORT).show()
                 TODO("Not yet implemented")
             }
 
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+
+
+            }
         }
 
-    }
+
+
+
+        }
+
 
 
 }
