ed93845078d4938e6e0376704a62b7035a68a568
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-03 16:09:33 +0500

    add picture

--- app/src/main/java/com/example/androidcore/MainActivity.kt
+++ app/src/main/java/com/example/androidcore/MainActivity.kt
@@ -17,10 +17,10 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+    }
 }
